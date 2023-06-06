49888e5dbfc8efaf91591500c00f77a72863cf4e
cbrooksacs <132284057+cbrooksacs@users.noreply.github.com>
2023-06-06 14:32:25 -0400

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,18 +2,6 @@ module.exports = {
   ci: {
     collect: {
       staticDistDir: "./",
-    },
-    assert: {
-      // assert options here
-    },
-    upload: {
-      // upload options here
-    },
-    server: {
-      // server options here
-    },
-    wizard: {
-      // wizard options here
-    },
+    }
   },
 };
